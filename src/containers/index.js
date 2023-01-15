94ae15437399924a573d528d375da7e71ea0cbec
Eyitayo Itunu Babatope <112722909+eyitayoit-alt@users.noreply.github.com>
2023-01-15 17:46:49 +0100

    Create index.js

---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+export {default as Blog} from './Blog';
+export {default as Possibility} from './Possibility';
+export {default as Header} from './Header';
+export {default as Footer} from './Footer';
+export {default as WhatGP3} from './WhatGP3';
+export {default as Features} from './Features';
