75f0eb211dcc071e30a18e7fd91ff2d6a52a1a94
Eyitayo Itunu Babatope <112722909+eyitayoit-alt@users.noreply.github.com>
2023-01-15 17:52:43 +0100

    Update index.js

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,6 +1,6 @@
-export {default as Blog} from './blog/Blog';
-export {default as Possibility} from './possibility/Possibility';
-export {default as Header} from './header/Header';
-export {default as Footer} from './footer/Footer';
-export {default as WhatGP3} from './whatGP3/WhatGP3';
-export {default as Features} from './features/Features';
+export {default as Blog} from './Blog';
+export {default as Possibility} from './Possibility';
+export {default as Header} from './Header';
+export {default as Footer} from './Footer';
+export {default as WhatGP3} from './WhatGP3';
+export {default as Features} from './Features';
