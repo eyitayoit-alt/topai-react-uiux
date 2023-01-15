5f64b8d8d472f7bbc95c89c0476a837e897cc9bd
Eyitayo Itunu Babatope <112722909+eyitayoit-alt@users.noreply.github.com>
2023-01-15 17:35:06 +0100

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,3 @@
-export {default as Blog} from './blog/Blog';
-export {default as Possibility} from './possibility/Possibility';
-export {default as Header} from './header/Header';
-export {default as Footer} from './footer/Footer';
-export {default as WhatGP3} from './whatGP3/WhatGP3';
-export {default as Features} from './features/Features';
+export {default as Brand } from "./brand/Brand";
+export {default as Navbar } from "./navbar/Navbar";
+export {default as CTA } from "./cta/CTA";
